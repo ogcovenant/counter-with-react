4d3b50fc3ebe0097cc6a97b260386acf469fb926
Covenant Ogowale <justcovenant@outlook.com>
2024-01-24 09:33:23 +0100

    Finished the functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count+1);
+  }
+  const decrement = () => {
+    if(count != 0) setCount(count-1);
+  }
+  const reset = () => {
+    setCount(0);
+  }
   return (
     <>
-      
+      <h1>{count}</h1>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={decrement}>-</button>
+      </div>
     </>
   );
 }
